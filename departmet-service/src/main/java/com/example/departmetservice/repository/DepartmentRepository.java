ceab4ddb273f22be20bfebdf8fffc5ad684a46ce
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-08-15 22:34:15 +0530

    add department repository

---
+++ departmet-service/src/main/java/com/example/departmetservice/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.example.departmetservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.departmetservice.entity.Department;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
