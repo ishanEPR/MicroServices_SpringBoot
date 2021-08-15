74a0cbaf2cb913f85967b5e87116024e056ac0e5
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-08-15 23:02:21 +0530

    add save department

---
+++ departmet-service/src/main/java/com/example/departmetservice/controller/DepartmentController.java
@@ -0,0 +1,28 @@
+package com.example.departmetservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.departmetservice.entity.Department;
+import com.example.departmetservice.service.DepartmentService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/departments")
+@Slf4j
+public class DepartmentController {
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	@PostMapping("/")
+	public Department saveDepartment(@RequestBody Department department) {
+		log.info("Inside saveDepartment method of DepartmentController");
+		return departmentService.saveDepartment(department);
+	}
+
+}
--- departmet-service/src/main/java/com/example/departmetservice/service/DepartmentService.java
+++ departmet-service/src/main/java/com/example/departmetservice/service/DepartmentService.java
@@ -3,12 +3,22 @@ package com.example.departmetservice.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.departmetservice.entity.Department;
 import com.example.departmetservice.repository.DepartmentRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class DepartmentService {
 	
 	@Autowired
 	private DepartmentRepository departmentRepository;
+
+	public Department saveDepartment(Department department) {
+		log.info("Inside saveDepartment method of DepartmentService");
+		
+		return departmentRepository.save(department);
+	}
 
 }
